ded3c5d38f86afa9ecff97e8f7cc79f79b8209b7
Osinachi Chukwujama <osinachi.chukwujama@gmail.com>
2024-06-21 19:21:14 +0100

    chore: remove database deps from app.py

--- newapp.py
+++ newapp.py
@@ -1,30 +1,27 @@
 from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from config import Config
 
 app = Flask(__name__)
+app.config.from_object(Config)
 
-# Dummy data for blog posts
-posts = [
-    {
-        'id': 1,
-        'title': 'First Post',
-        'content': 'This is the content of the first post.'
-    },
-    {
-        'id': 2,
-        'title': 'Second Post',
-        'content': 'This is the content of the second post.'
-    }
-]
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(120), nullable=False)
+    content = db.Column(db.Text, nullable=False)
 
 @app.route('/')
 def index():
+    posts = Post.query.all()
     return render_template('index.html', posts=posts)
 
 @app.route('/post/<int:post_id>')
 def post(post_id):
-    post = next((post for post in posts if post['id'] == post_id), None)
-    if post is None:
-        return "Post not found!", 404
+    post = Post.query.get_or_404(post_id)
     return render_template('post.html', post=post)
 
 @app.route('/new', methods=['GET', 'POST'])
@@ -32,8 +29,9 @@ def new_post():
     if request.method == 'POST':
         title = request.form['title']
         content = request.form['content']
-        new_id = len(posts) + 1
-        posts.append({'id': new_id, 'title': title, 'content': content})
+        new_post = Post(title=title, content=content)
+        db.session.add(new_post)
+        db.session.commit()
         return redirect(url_for('index'))
     return render_template('new_post.html')
 
