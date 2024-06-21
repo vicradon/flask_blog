838d5a943ce989b5bf5eba969a6460884f0d1cda
Osinachi Chukwujama <osinachi.chukwujama@gmail.com>
2024-06-21 19:30:07 +0100

    chore: update app with db details

--- app_without_db_conn.py
+++ app_without_db_conn.py
@@ -1,27 +1,30 @@
 from flask import Flask, render_template, request, redirect, url_for
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
-from config import Config
 
 app = Flask(__name__)
-app.config.from_object(Config)
 
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
-
-class Post(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(120), nullable=False)
-    content = db.Column(db.Text, nullable=False)
+# Dummy data for blog posts
+posts = [
+    {
+        'id': 1,
+        'title': 'First Post',
+        'content': 'This is the content of the first post.'
+    },
+    {
+        'id': 2,
+        'title': 'Second Post',
+        'content': 'This is the content of the second post.'
+    }
+]
 
 @app.route('/')
 def index():
-    posts = Post.query.all()
     return render_template('index.html', posts=posts)
 
 @app.route('/post/<int:post_id>')
 def post(post_id):
-    post = Post.query.get_or_404(post_id)
+    post = next((post for post in posts if post['id'] == post_id), None)
+    if post is None:
+        return "Post not found!", 404
     return render_template('post.html', post=post)
 
 @app.route('/new', methods=['GET', 'POST'])
@@ -29,9 +32,8 @@ def new_post():
     if request.method == 'POST':
         title = request.form['title']
         content = request.form['content']
-        new_post = Post(title=title, content=content)
-        db.session.add(new_post)
-        db.session.commit()
+        new_id = len(posts) + 1
+        posts.append({'id': new_id, 'title': title, 'content': content})
         return redirect(url_for('index'))
     return render_template('new_post.html')
 
